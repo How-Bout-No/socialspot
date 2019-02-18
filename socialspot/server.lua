4b34d146905f9219a5024f0a0b3163a3b5131c46
thefa <thefalconpunch@icloud.com>
2019-02-18 14:06:46 -0500

    Update autoupdater

--- socialspot/server.lua
+++ socialspot/server.lua
@@ -5,14 +5,11 @@ autoupdatever = true  -- automatically detect and fetch the download for the lat
 --Startup
 print('\n\27[104m> SocialSpot <\27[0m')
 _version = "0.1"
-print("\27[94;107mChecking For Updates...")
+print("\27[94mChecking For Updates...")
 PerformHttpRequest("https://textuploader.com/15d0x/raw", function(err, rText, headers)
-if rText > _version then
-	print("An update is available!")
-else
+if rText <= _version then
 	print("You are up to date")
 end
-print("\27[0m")
 end, "GET", "", {table = nil})
 --End Startup
 
@@ -55,16 +52,13 @@ end)
 if autoupdatever == true then
 PerformHttpRequest("https://textuploader.com/15d0x/raw", function(err, rText, headers)
 		if rText > _version then
-			print("\n\nOutdated Version!\nThe download for the latest version of TextScript will now be opened!\nPlease install the new version!")
+			print("\n\nOutdated Version!\nDownloading latest release...")
 			os.execute("start https://github.com/How-Bout-No/socialspot/archive/master.zip")
-		elseif _version > rText then
-			print("\n\nUnknown Version!\nThe download for the latest version of TextScript will now be opened!\nPlease install the new version!")
-			os.execute("start https://github.com/How-Bout-No/socialspot/archive/master.zip")
-		else
-			print("TextScript is OK\n")
 		end
+		print("\27[0m")
 end, "GET", "", {table = nil})
 else
-print("Automatic updates are disabled. Please ensure your version matches the current version manually.")
+print("Automatic updates are disabled.")
+print("\27[0m")
 end
 --End AutoUpdater
