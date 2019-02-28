06cb14917c352060b37731ce5304ce57a65423ad
thefa <thefalconpunch@icloud.com>
2019-02-27 20:39:48 -0500

    Make everything big again

--- socialspot/html/listener.js
+++ socialspot/html/listener.js
@@ -39,7 +39,9 @@ $(function()
 					.appendTo('.users');
 				});
 			}
+			console.log(Object.keys(data));
 			var username = data.user;
+			console.log(username);
 			var usercolor = $.parseJSON(data.color);
 			var curpm = '';
 			$('.users').on('click', 'li', function(){
